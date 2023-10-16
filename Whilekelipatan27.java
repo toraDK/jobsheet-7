666a7d2feaa110ec9ceaa26962372fc9d6230079
tora180405 <tora@gmail.com>
2023-10-16 15:59:55 +0700

    upload file Whilekelipatan27.java

---
+++ Whilekelipatan27.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Whilekelipatan27 {
+    public static void main(String[] args) {
+        Scanner scan27 = new Scanner(System.in);
+
+        int kelipatan, total = 0, counter = 0, i = 1;
+        double rata2;
+
+        System.out.println("Masukkan bilangan kelipatan (1-9): ");
+        kelipatan = scan27.nextInt();
+
+        // for(int i = 1; i <= 50; i++){
+        //     if(i % kelipatan == 0){
+        //         total += i;
+        //         counter++;
+        //     }
+        // }
+
+        while (i <= 50) {
+            if(i % kelipatan == 0){
+                total += i;
+                counter++;
+            }
+            i++;
+        }
+        if(counter > 0){
+            rata2 = (double) total/counter;
+            System.out.println("rata ratanya adalah " + rata2);
+        }else{
+            System.out.println("rata rata tidak ditemukan");
+        }
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter); 
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);  
+    }
+}
